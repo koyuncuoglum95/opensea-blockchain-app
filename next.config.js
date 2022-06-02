665d1b61e1f288e77415f739089b2bd5cb177d9a
koyuncuoglum95 <77508734+koyuncuoglum95@users.noreply.github.com>
2022-06-02 05:37:58 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com','gateway.ipfscdn.io']
+    domains: ['lh3.googleusercontent.com','gateway.ipfscdn.io','opensea-blockchain-app-nine.vercel.app']
   }
 }
 
