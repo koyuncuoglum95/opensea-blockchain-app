1a1def67711bbb7eca93e2c6ca6a048bcda5dbe0
koyuncuoglum95 <77508734+koyuncuoglum95@users.noreply.github.com>
2022-06-02 05:51:23 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['lh3.googleusercontent.com','gateway.ipfscdn.io']
+    domains: ['lh3.googleusercontent.com','gateway.ipfscdn.io','assets.vercel.com'],
+    formats: ['image/avif', 'image/webp'],
   }
 }
 
